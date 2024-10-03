a0b19399cf2440bcd92430725d7452dca85da3ed
Kaua <rodrigueskaua76@gmai.com>
2024-10-03 11:33:10 -0300

    criando array, obejetos e metodos

--- script.js
+++ script.js
@@ -23,10 +23,27 @@
 
 function Ola(nome){
     //strings literal ou template string ${} chama a varialvel
-    alert(`seja bem vindo a ${nome}`)
+    // alert(`seja bem vindo a ${nome}`)
     console.log(`Seja bem vindo a ${nome}`) 
     document.write(`seja bem vindo a ${nome}`)
 }
 Ola("Fiap")
 
+//arrow function
+
+const hello=() =>(console.log('função arrow function'))
+hello()
+
+//ARRAY[], OBJETOS{} E METODOS()
+
+let jogadores= [
+    {nome: 'Huguinho', idade:17},
+    {nome: 'Zezinho', idade: 18},
+    {nome: 'Luizinho', idade: 19}
+]
+
+let listarJogadores = jogadores.map(function(item){
+    return item.nome;
+})
+console.log(listarJogadores)
     
